6cf3331aafbd7b0ecd2a3bda066ab1579feae072
mbajur <mbajur@gmail.com>
2024-11-23 11:50:56 +0100

    UI improvements

--- app/controllers/inner_performance/dashboard_controller.rb
+++ app/controllers/inner_performance/dashboard_controller.rb
@@ -3,8 +3,12 @@ module InnerPerformance
     def index
       @q = InnerPerformance::Event.all.ransack(params[:q])
       @recent_events = @q.result.page(params[:page])
+
       @average_duration = InnerPerformance::Event.all.average(:duration)
-      @biggest_events = InnerPerformance::Event.select('SUM(duration) as duration, name, COUNT(*) as count').group(:name).order('duration DESC')
+      @biggest_events = InnerPerformance::Event.select('SUM(duration) as duration, name, COUNT(*) as count')
+                                               .group(:name)
+                                               .order('duration DESC')
+                                               .limit(10)
     end
   end
 end
