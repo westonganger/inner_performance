d939c164f745e3e798f815ab27fa9947f56b9c67
mbajur <mbajur@gmail.com>
2024-11-23 11:53:39 +0100

    Apply default sorting

--- app/controllers/inner_performance/dashboard_controller.rb
+++ app/controllers/inner_performance/dashboard_controller.rb
@@ -2,6 +2,7 @@ module InnerPerformance
   class DashboardController < ApplicationController
     def index
       @q = InnerPerformance::Event.all.ransack(params[:q])
+      @q.sorts = "created_at desc" if @q.sorts.empty?
       @recent_events = @q.result.page(params[:page])
 
       @average_duration = InnerPerformance::Event.all.average(:duration)
