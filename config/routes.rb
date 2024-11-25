98e7a97290adead4972ae57a6b87d63917899065
Weston Ganger <weston@westonganger.com>
2024-11-25 14:32:51 -0800

    Remove engine mount from within engine routes (#8)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 InnerPerformance::Engine.routes.draw do
-  mount InnerPerformance::Engine, at: '/performance'
-
   resources :events, only: [:index]
 
   root to: 'dashboard#index'
